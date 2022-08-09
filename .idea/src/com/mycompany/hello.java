1d05c82706ca495e5072ce6dfba62220fca04a05
Mehdi <medmehdielmanaa@gmailcom>
2022-08-09 23:27:36 +0200

    initial commit

---
+++ .idea/src/com/mycompany/hello.java
@@ -0,0 +1,90 @@
+package com.mycompany;
+
+public class hello {
+
+
+    /**
+     * Hello world!
+     *
+     */
+    public class App
+    {
+        public static void main( String[] args )
+        {   /**String phrase="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de ";
+         int pl= 8;
+         System.out.println( phrase + pl);
+         phrase = "il y a quelques années cependant, elles étaient au nombre de ";
+         int pL= 9;
+         System.out.println(  phrase +  pL);
+         String planete= "nombre Total des planetes a augmente";
+         System.out.println(planete);
+
+         int resultat= 5/2;
+         int reste = 5%2;
+         System.out.println(resultat + reste);
+         float chiffre=5f/2+3;
+         float chiffre2=5f/(2+3);
+         System.out.println(chiffre +chiffre2);
+
+         int n1=20;
+         int n2=30;
+         char lettre1= 'Y';
+         char lettre2= 'f';
+         boolean estCeQUE= n1==n2;
+         boolean estCeQUE2= n1!=n2;
+         boolean estCeQUE3= n1>=n2;
+         boolean estCeQUE4= lettre1<=lettre2;
+         System.out.println(estCeQUE);
+         System.out.println(estCeQUE2);
+         System.out.println(estCeQUE3);
+         System.out.println(estCeQUE4);
+         boolean bool1= true;
+         boolean bool2= false;
+         boolean bool3= false;
+         boolean estCeQUE= bool1||bool2;
+         boolean estCeQUE2= bool1&&bool2;
+         boolean estCeQUE3= bool3^bool2;
+         boolean estCeQUE4= bool3^bool1;
+         boolean estCeQUE5= !bool3^bool1;
+         System.out.println(estCeQUE);
+         System.out.println(estCeQUE2);
+         System.out.println(estCeQUE3);
+         System.out.println(estCeQUE4);
+         System.out.println(estCeQUE5);
+
+         int valeur1= 10;
+         int valeur2;
+         int valeur3=++valeur1;
+         valeur1= valeur1 ++;
+         valeur2= valeur1 ++;
+
+         System.out.println(valeur1);
+         System.out.println(valeur2);
+         System.out.println(valeur3);
+         int age=55;
+         System.out.printf("l age de la personne est de %d ",age);
+
+         if (age>30) {
+         System.out.println("oui je fais plus jeune.");
+         }
+         int ageDuChef = 28;
+         if (ageDuChef > 20 && age < 50){
+         System.out.println("l age du Chef est "+ ageDuChef);
+         }*/
+            int numeroannee=2003;
+            int nbPlanetes=8;
+            if (numeroannee==2003) {
+
+                System.out.printf("En %d, les planètes du système solaire étaient au nombre de ", numeroannee +nbPlanetes) ;
+            }
+            numeroannee=2014;
+            if (numeroannee==2014) {
+                System.out.printf("En %d, les planètes du système solaire étaient au nombre de  ", numeroannee+ nbPlanetes++) ;
+
+            }
+        }
+
+    }
+
+
+}
